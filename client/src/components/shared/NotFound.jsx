dec08e12a2f678a5a100a30dda9d110375810e98
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-23 21:23:51 +0600

    not found component

---
+++ client/src/components/shared/NotFound.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import NotFoundImg from '../../assets/gifs/NotFound.gif';
+
+const NotFound = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className='hero min-h-screen'>
+      <div className='hero-content text-center'>
+        <div className='max-w-md container mx-auto flex flex-col items-center'>
+          <img src={NotFoundImg} alt='not found' />
+          <h1 className='text-3xl font-bold py-4'>Page is not found!</h1>
+          <button
+            className='btn bg-main text-white hover:bg-white hover:text-black hover:border-main mr-2'
+            onClick={() => navigate('/')}
+          >
+            Go to Home
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
