997b269164f86370832fc02d5b2419c284c19552
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-23 11:53:44 +0600

    tailwind config modifications

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        main: '#2273B8',
+        second: '#47019d',
+        three: '#e00256',
+        // black: '#212121',
+        black: '#454545',
+        white: '#ffffff',
+        gray: '#808080e2',
+      },
+      fontFamily: {
+        oswald: ['Oswald', 'sans-serif'],
+        lobster: ['Lobster', 'cursive'],
+      },
+      lineHeight: {
+        // 'extra-loose': '2.5',
+        // 12: '3rem',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
