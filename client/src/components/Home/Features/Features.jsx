cebc2c10b37c91b3ed542a1ef3d4f35be11fb21a
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-23 21:45:41 +0600

    features component

---
+++ client/src/components/Home/Features/Features.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import medicalTreatmentImg from '../../../assets/images/treatment.png';
+import emergencyHelpImg from '../../../assets/images/emergency.png';
+import regularExaminationImg from '../../../assets/images/examination.png';
+import FeaturesCard from './FeaturesCard';
+
+const Features = () => {
+  const data = [
+    {
+      name: 'Medical Treatment',
+      description:
+        'Management and care of a patient to combat disease or disorder.',
+      image: `${medicalTreatmentImg}`,
+    },
+    {
+      name: 'Emergency Help',
+      description:
+        'We have special equipment to give emergency help if/when necessary.',
+      image: `${emergencyHelpImg}`,
+    },
+    {
+      name: 'Regular Examination',
+      description: 'Make online appointments for regular health examinations.',
+      image: `${regularExaminationImg}`,
+    },
+  ];
+
+  return (
+    <div className='container mx-auto my-10 p-3 min-h-screen'>
+      <div className='flex flex-col lg:flex-row justify-center md:justify-around container mx-auto my-6'>
+        {data.map((feature, index) => (
+          <FeaturesCard key={index} feature={feature} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Features;
