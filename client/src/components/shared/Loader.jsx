a877cedb2bdede0228dfc238996297af9b5d0d1b
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-23 21:26:01 +0600

    loading component

---
+++ client/src/components/shared/Loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import loaderImg from '../../assets/gifs/loadin.gif';
+
+const Loader = () => {
+  return (
+    <div className='hero min-h-screen'>
+      <div className='hero-content text-center'>
+        <div className='max-w-md container mx-auto flex flex-col items-center'>
+          <img src={loaderImg} alt='loader' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
