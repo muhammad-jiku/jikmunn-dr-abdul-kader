c3a25f01ed091d5f4c038cbd92a53d3ea3f47495
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-23 21:48:10 +0600

    features section added to homepage

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Navbar } from './shared/Navbar';
 export { default as Footer } from './shared/Footer';
 export { default as NotFound } from './shared/NotFound';
 export { default as Banner } from './Home/Banner/Banner';
+export { default as Features } from './Home/Features/Features';
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Banner } from '../../components';
+import { Banner, Features } from '../../components';
 
 const HomePage = () => {
   return (
     <>
       <Banner />
+      <Features />
     </>
   );
 };
