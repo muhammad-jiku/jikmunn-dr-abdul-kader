244d26f50afa0b9b0aa81d8016903187c792325e
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-26 15:47:38 +0600

    store init

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { appointmentStore } from './utils/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={appointmentStore}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
