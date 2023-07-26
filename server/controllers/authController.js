769a886291956f1e763dc4d4d45fff80e5d285cd
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-26 10:43:49 +0600

    google auth call back func

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const generateToken = (user) => {
   return jwt.sign(
@@ -10,4 +11,48 @@ const generateToken = (user) => {
       expiresIn: `${process.env.EXPIRES_IN}s`,
     }
   );
+};
+
+const googleAuthCallback = (req, res) => {
+  // This callback function will handle the Google OAuth authentication callback
+  // Extract the user data from req.user obtained by Passport.js
+  const { id, displayName, emails } = req.user;
+  const email = emails[0].value;
+
+  // Check if the user already exists in the database based on the Google ID
+  User.findOne({ googleId: id }, async (err, user) => {
+    if (err) {
+      return res.status(500).json({
+        message: 'Internal server error',
+      });
+    }
+
+    if (user) {
+      // User already exists, generate a JWT token and send it as a response
+      const token = generateToken(user);
+      return res.redirect(`/signin?token=${token}`);
+    } else {
+      // User does not exist, create a new user with the Google profile data
+      const newUser = new User({
+        googleId: id,
+        email,
+        username: displayName.toLowerCase(),
+      });
+
+      try {
+        await newUser.save();
+        // Generate a JWT token and send it as a response
+        const token = generateToken(newUser);
+        return res.redirect(`/signin?token=${token}`);
+      } catch (error) {
+        return res.status(500).json({
+          message: 'Internal server error',
+        });
+      }
+    }
+  });
+};
+
+module.exports = {
+  googleAuthCallback,
 };
