d44dba3fb1312b241aa91d324db3d57653832235
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-28 20:16:08 +0600

    authenticate check middleware

---
+++ server/middlewares/auth/AuthHandler.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../models/User');
+const AsyncError = require('../errors/AsyncError');
+const ErrorHandler = require('../errors/ErrorHandler');
+
+const isAuthenticated = AsyncError(async (req, res, next) => {
+  const authHeader = await req?.headers?.authorization;
+  // console.log('Auth header........', authHeader);
+  if (!authHeader) {
+    // return next(new ErrorHandler('Please Login to access this resource', 401));
+    return next(new ErrorHandler('Please Login to access this resource', 400));
+  }
+  const token = await authHeader?.split(' ')[1];
+  console.log('token.........', token);
+  if (!token) {
+    // return next(new ErrorHandler('Please Login to access this resource', 401));
+    return next(new ErrorHandler('Please Login to access this resource', 400));
+  } else {
+    jwt.verify(token, process.env.SECRET_KEY, async (err, decoded) => {
+      console.log(decoded);
+      if (err) {
+        console.log(err);
+        return next(new ErrorHandler('Access to this route is forbidden', 403));
+      }
+      req.decoded = decoded;
+      console.log('decoded: ', decoded);
+      req.user = await User.findById(decoded.id);
+      console.log('user', req.user);
+
+      next();
+    });
+  }
+});
+
+module.exports = {
+  isAuthenticated,
+};
