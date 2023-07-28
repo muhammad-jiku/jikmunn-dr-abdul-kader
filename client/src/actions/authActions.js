527f8cdfb9be8f67fc527b6a7d82d10f8a2c5a43
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-29 00:43:28 +0600

    sign in actions

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import {
   CLEAR_ERRORS,
+  SIGNIN_AUTH_FAILURE,
+  SIGNIN_AUTH_REQUEST,
+  SIGNIN_AUTH_SUCCESS,
   SIGNUP_AUTH_FAILURE,
   SIGNUP_AUTH_REQUEST,
   SIGNUP_AUTH_SUCCESS,
 } from '../constants/authConstant';
 
-// Register
 export const signUpUser = (userData) => async (dispatch) => {
   try {
     await dispatch({
@@ -29,6 +31,33 @@ export const signUpUser = (userData) => async (dispatch) => {
   } catch (error) {
     await dispatch({
       type: SIGNUP_AUTH_FAILURE,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const signInUser = (userData) => async (dispatch) => {
+  try {
+    await dispatch({
+      type: SIGNIN_AUTH_REQUEST,
+    });
+
+    const config = {
+      headers: { 'content-type': 'application/json' },
+    };
+
+    const { data } = await axios.post(`/api/v1/auth/signin`, userData, config);
+
+    await dispatch({
+      type: SIGNIN_AUTH_SUCCESS,
+      payload: data?.data,
+    });
+
+    const token = await data?.token;
+    localStorage?.setItem('token', token);
+  } catch (error) {
+    await dispatch({
+      type: SIGNIN_AUTH_FAILURE,
       payload: error.response.data.message,
     });
   }
