37d5943d738ced7183edb38c74e9da14e92559c8
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-28 20:01:57 +0600

    error checker middleware

---
+++ server/middlewares/errors/ErrorChecker.js
@@ -0,0 +1,42 @@
+const ErrorHandler = require('./ErrorHandler');
+
+module.exports = (err, req, res, next) => {
+  // console.log(err);
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || 'Internal Server Error';
+
+  // Wrong Mongodb Id error
+  if (err.name === 'CastError') {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    // console.log(message);
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    // console.log(message);
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Wrong JWT error
+  if (err.name === 'JsonWebTokenError') {
+    const message = `Json Web Token is invalid, Try again `;
+    // console.log(message);
+    err = new ErrorHandler(message, 400);
+  }
+
+  // JWT EXPIRE error
+  if (err.name === 'TokenExpiredError') {
+    const message = `Json Web Token is Expired, Try again `;
+    // console.log(message);
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    // message: err ,
+    message: err.stack,
+    // message: err.message,
+  });
+};
