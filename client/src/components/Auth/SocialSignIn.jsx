58aee2b521698c1ebf1b95209852eaa165da7dd7
muhammad-jiku <muhammadjiku364@gmail.com>
2023-08-05 01:02:59 +0600

    google sign in api func added

--- client/src/components/Auth/SocialSignIn.jsx
+++ client/src/components/Auth/SocialSignIn.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
+import {
+  // GoogleLogin,
+  useGoogleLogin,
+} from '@react-oauth/google';
+import axios from 'axios';
+// import jwtDecode from 'jwt-decode';
 
 const SocialSignIn = () => {
+  const googleLogin = useGoogleLogin({
+    flow: 'auth-code',
+    onSuccess: async ({ code }) => {
+      const tokens = await axios.post(
+        'http://localhost:5000/api/v1/auth/google',
+        {
+          code,
+        }
+      );
+
+      console.log(tokens);
+    },
+
+    onError: (errorResponse) => console.log(errorResponse),
+  });
   const handleGoogleLogin = async () => {
-    await console.log('google sign in');
+    await console.log('google sign in check!');
+    await googleLogin();
   };
 
   return (
@@ -15,6 +37,16 @@ const SocialSignIn = () => {
       >
         Continue with Google <FcGoogle />
       </button>
+      {/* <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log(credentialResponse);
+          const USER_CREDENTIAL = jwtDecode(credentialResponse.credential);
+          console.log(USER_CREDENTIAL);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      /> */}
     </>
   );
 };
