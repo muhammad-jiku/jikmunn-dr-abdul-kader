1ac67b252c2c925bbe141e08f3664142d094b173
muhammad-jiku <muhammadjiku364@gmail.com>
2023-08-06 11:27:29 +0600

    user details route init

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -4,7 +4,9 @@ const {
   signIn,
   googleSignIn,
   signOut,
+  getUserDetails,
 } = require('../controllers/authController');
+const { isAuthenticated } = require('../middlewares/auth/AuthHandler');
 
 const authRoute = express.Router({
   caseSensitive: true,
@@ -14,5 +16,6 @@ authRoute.route('/signup').post(signUp);
 authRoute.route('/signin').post(signIn);
 authRoute.route('/google').post(googleSignIn);
 authRoute.route('/signout').post(signOut);
+authRoute.route('/me').get(isAuthenticated, getUserDetails);
 
 module.exports = authRoute;
