73e44f4878ad6fff77a6a022058ae73eb5d44c58
muhammad-jiku <muhammadjiku364@gmail.com>
2023-08-09 22:57:51 +0600

    service action added

---
+++ client/src/actions/serviceActions.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import {
+  NEW_SERVICE_FAILURE,
+  NEW_SERVICE_REQUEST,
+  NEW_SERVICE_SUCCESS,
+} from '../constants/serviceConstant';
+
+export const addNewService = (serviceData) => async (dispatch) => {
+  try {
+    await dispatch({
+      type: NEW_SERVICE_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        authorization: `Bearer ${localStorage?.getItem('token')}`,
+        'content-type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.post(
+      `/api/v1/admin/create-service`,
+      serviceData,
+      config
+    );
+
+    await dispatch({
+      type: NEW_SERVICE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    await dispatch({
+      type: NEW_SERVICE_FAILURE,
+      payload: error.response.data.message,
+    });
+  }
+};
