ce2832dd64200ee1f4e63c247aac47d530c917c5
muhammad-jiku <muhammadjiku364@gmail.com>
2023-08-10 00:42:41 +0600

    price reducer stored

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -3,11 +3,13 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { authReducer, profileReducer } from '../reducers/authReducer';
 import { newServiceReducer } from '../reducers/serviceReducer';
+import { newPriceReducer } from '../reducers/priceReducer';
 
 const reducer = combineReducers({
   user: authReducer,
   profile: profileReducer,
   newService: newServiceReducer,
+  newPrice: newPriceReducer,
 });
 
 let initialState = {};
