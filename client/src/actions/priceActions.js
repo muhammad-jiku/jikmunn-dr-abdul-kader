82e22d11e0e261d8f405f221ce85da9345448137
muhammad-jiku <muhammadjiku364@gmail.com>
2023-08-10 22:25:43 +0600

    admin all prices action func added

--- client/src/actions/priceActions.js
+++ client/src/actions/priceActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import {
+  ADMIN_ALL_PRICES_FAILURE,
+  ADMIN_ALL_PRICES_REQUEST,
+  ADMIN_ALL_PRICES_SUCCESS,
   CLEAR_ERRORS,
   NEW_PRICE_FAILURE,
   NEW_PRICE_REQUEST,
@@ -32,6 +35,33 @@ export const addNewPrice = (priceData) => async (dispatch) => {
   } catch (error) {
     await dispatch({
       type: NEW_PRICE_FAILURE,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const adminAllPrices = () => async (dispatch) => {
+  try {
+    await dispatch({
+      type: ADMIN_ALL_PRICES_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        authorization: `Bearer ${localStorage?.getItem('token')}`,
+        'content-type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.get(`/api/v1/admin/prices`, config);
+
+    await dispatch({
+      type: ADMIN_ALL_PRICES_SUCCESS,
+      payload: data?.data,
+    });
+  } catch (error) {
+    await dispatch({
+      type: ADMIN_ALL_PRICES_FAILURE,
       payload: error.response.data.message,
     });
   }
