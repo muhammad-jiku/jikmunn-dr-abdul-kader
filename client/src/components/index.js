7fea2ad9275be43f737743e86807ebd05c172037
muhammad-jiku <muhammadjiku364@gmail.com>
2023-08-12 23:24:20 +0600

    update service page component added

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -30,6 +30,7 @@ export { default as Profile } from './Dashboard/Profile';
 export { default as UpdatePassword } from './Dashboard/UpdatePassword';
 export { default as NewService } from './Dashboard/Admin/ManageServices/NewService';
 export { default as AllServices } from './Dashboard/Admin/ManageServices/AllServices';
+export { default as UpdateService } from './Dashboard/Admin/ManageServices/UpdateService';
 export { default as NewPrice } from './Dashboard/Admin/ManagePrices/NewPrice';
 export { default as AllPrices } from './Dashboard/Admin/ManagePrices/AllPrices';
 export { default as AllUsers } from './Dashboard/Admin/ManageUsers/AllUsers';
---
+++ client/src/pages/DashboardPages/Admin/Services/UpdateServicePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { UpdateService } from '../../../../components';
+
+const UpdateServicePage = () => {
+  return (
+    <>
+      <UpdateService />
+    </>
+  );
+};
+
+export default UpdateServicePage;
