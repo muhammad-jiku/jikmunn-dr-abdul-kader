07a1e8b382850e67f52b785e40e8a7454971a1c3
muhammad-jiku <muhammadjiku364@gmail.com>
2023-08-30 23:46:50 +0600

    stripe api key send controller func added

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -2,21 +2,40 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const AsyncError = require('../middlewares/errors/AsyncError');
 
 const processPayment = AsyncError(async (req, res) => {
-  const myPayment = await stripe.paymentIntents.create({
-    amount: req.body.amount,
-    currency: 'usd',
-    payment_method_types: ['card'],
-    metadata: {
-      company: 'Dr. Abdul Kader',
-    },
-  });
+  try {
+    const myPayment = await stripe.paymentIntents.create({
+      amount: req.body.amount,
+      currency: 'usd',
+      payment_method_types: ['card'],
+      metadata: {
+        company: 'Dr. Abdul Kader',
+      },
+    });
 
-  return res.status(200).json({
-    success: true,
-    client_secret: myPayment.client_secret,
-  });
+    return res.status(200).json({
+      success: true,
+      client_secret: myPayment.client_secret,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Invalid payment',
+    });
+  }
+});
+
+const sendStripeApiKey = AsyncError(async (req, res) => {
+  try {
+    return res.status(200).json({
+      stripeApiKey: process.env.STRIPE_API_KEY,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Invalid API Key',
+    });
+  }
 });
 
 module.exports = {
   processPayment,
+  sendStripeApiKey,
 };
