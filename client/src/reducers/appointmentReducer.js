6782ac932ca505234af7a5cc0a84c718cad27ca0
muhammad-jiku <muhammadjiku364@gmail.com>
2023-09-03 20:28:30 +0600

    admin all appointments reducer added

--- client/src/reducers/appointmentReducer.js
+++ client/src/reducers/appointmentReducer.js
@@ -2,6 +2,9 @@ import {
   ADD_APPOINTMENT_FAILURE,
   ADD_APPOINTMENT_REQUEST,
   ADD_APPOINTMENT_SUCCESS,
+  ADMIN_ALL_APPOINTMENTS_FAILURE,
+  ADMIN_ALL_APPOINTMENTS_REQUEST,
+  ADMIN_ALL_APPOINTMENTS_SUCCESS,
   ALL_APPOINTMENTS_FAILURE,
   ALL_APPOINTMENTS_REQUEST,
   ALL_APPOINTMENTS_SUCCESS,
@@ -55,6 +58,38 @@ export const allAppointmentsReducer = (
       };
 
     case ALL_APPOINTMENTS_FAILURE:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const adminAllAppointmentsReducer = (
+  state = { appointments: [] },
+  action
+) => {
+  switch (action.type) {
+    case ADMIN_ALL_APPOINTMENTS_REQUEST:
+      return {
+        loading: true,
+      };
+
+    case ADMIN_ALL_APPOINTMENTS_SUCCESS:
+      return {
+        loading: false,
+        appointments: action.payload,
+      };
+
+    case ADMIN_ALL_APPOINTMENTS_FAILURE:
       return {
         loading: false,
         error: action.payload,
