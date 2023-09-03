f78114ab03737f4ec76f89cee924c7b44627ec00
muhammad-jiku <muhammadjiku364@gmail.com>
2023-09-03 12:03:29 +0600

    booking route added

---
+++ server/routes/bookingRoute.js
@@ -0,0 +1,36 @@
+//  external import
+const express = require('express');
+const {
+  isAuthenticated,
+  authorizeAdmin,
+} = require('../middlewares/auth/AuthHandler');
+const {
+  createBooking,
+  getBookingData,
+  getAppointments,
+  getAdminAllAppointments,
+  updateAdminAppointmentData,
+  deleteAdminAppointment,
+} = require('../controllers/bookingController');
+
+const bookingRoute = express.Router({
+  caseSensitive: true,
+});
+
+bookingRoute.route('/booking/new').post(isAuthenticated, createBooking);
+
+bookingRoute.route('/booking/:id').get(isAuthenticated, getBookingData);
+
+bookingRoute.route('/bookings/me').get(isAuthenticated, getAppointments);
+
+bookingRoute
+  .route('/admin/bookings')
+  .get(isAuthenticated, authorizeAdmin, getAdminAllAppointments);
+
+bookingRoute
+  .route('/admin/booking/:id')
+  .put(isAuthenticated, authorizeAdmin, updateAdminAppointmentData)
+  .delete(isAuthenticated, authorizeAdmin, deleteAdminAppointment);
+
+//
+module.exports = bookingRoute;
