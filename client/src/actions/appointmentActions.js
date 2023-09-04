ff13795e38ae6ec91341b328c14cf94a27ca9345
muhammad-jiku <muhammadjiku364@gmail.com>
2023-09-04 11:53:59 +0600

    all appointments action func added

--- client/src/actions/appointmentActions.js
+++ client/src/actions/appointmentActions.js
@@ -3,6 +3,9 @@ import {
   ADD_APPOINTMENT_FAILURE,
   ADD_APPOINTMENT_REQUEST,
   ADD_APPOINTMENT_SUCCESS,
+  ALL_APPOINTMENTS_FAILURE,
+  ALL_APPOINTMENTS_REQUEST,
+  ALL_APPOINTMENTS_SUCCESS,
 } from '../constants/appointmentConstant';
 
 export const addAppointment = (appointment) => async (dispatch) => {
@@ -31,6 +34,33 @@ export const addAppointment = (appointment) => async (dispatch) => {
   } catch (error) {
     await dispatch({
       type: ADD_APPOINTMENT_FAILURE,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const allAppointments = () => async (dispatch) => {
+  try {
+    await dispatch({
+      type: ALL_APPOINTMENTS_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        authorization: `Bearer ${localStorage?.getItem('token')}`,
+        'content-type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.get('/api/v1/bookings/me', config);
+
+    await dispatch({
+      type: ALL_APPOINTMENTS_SUCCESS,
+      payload: data?.data,
+    });
+  } catch (error) {
+    await dispatch({
+      type: ALL_APPOINTMENTS_FAILURE,
       payload: error.response.data.message,
     });
   }
