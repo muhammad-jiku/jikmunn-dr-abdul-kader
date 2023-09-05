e6336a25a00789d4b8c6751f56577fb771bf2665
muhammad-jiku <muhammadjiku364@gmail.com>
2023-09-05 11:46:53 +0600

    admin all appointments page component

---
+++ client/src/pages/DashboardPages/Admin/Appointments/AllAppointments.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AllAppointments } from '../../../../components';
+
+const AllAppointmentsPage = () => {
+  return (
+    <>
+      <AllAppointments />
+    </>
+  );
+};
+
+export default AllAppointmentsPage;
