0c499379aeed992941f7d24bb64bec7fc27b67ac
muhammad-jiku <muhammadjiku364@gmail.com>
2023-09-07 12:27:16 +0600

    admin delete testimonial controller func added

--- server/controllers/testimonialController.js
+++ server/controllers/testimonialController.js
@@ -17,6 +17,32 @@ const getAllTestimonials = AsyncError(async (req, res) => {
   }
 });
 
+const deleteAdminTestimonial = AsyncError(async (req, res, next) => {
+  try {
+    const { id } = await req.params;
+    const testimonial = await Testimonial.findById({ _id: id });
+
+    if (!testimonial) {
+      return next(
+        new ErrorHandler(`Testimonial does not exist with Id: ${id}`, 400)
+      );
+    } else {
+      await Testimonial.deleteOne({ _id: id });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: 'Testimonial Deleted Successfully',
+    });
+  } catch (error) {
+    // console.log(error);
+    return res.status(500).json({
+      message: 'Internal Server Error',
+    });
+  }
+});
+
 module.exports = {
   getAllTestimonials,
+  deleteAdminTestimonial,
 };
