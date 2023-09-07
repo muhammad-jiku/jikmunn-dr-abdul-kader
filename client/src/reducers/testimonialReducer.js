4401a196bfa4424d9ae6fc69fa518d09c84e6ba1
muhammad-jiku <muhammadjiku364@gmail.com>
2023-09-07 18:08:12 +0600

    testimonial reducer added

---
+++ client/src/reducers/testimonialReducer.js
@@ -0,0 +1,112 @@
+import {
+  ADD_TESTIMONIAL_FAILURE,
+  ADD_TESTIMONIAL_REQUEST,
+  ADD_TESTIMONIAL_SUCCESS,
+  ALL_TESTIMONIALS_FAILURE,
+  ALL_TESTIMONIALS_REQUEST,
+  ALL_TESTIMONIALS_SUCCESS,
+  ADMIN_DELETE_TESTIMONIAL_FAILURE,
+  ADMIN_DELETE_TESTIMONIAL_REQUEST,
+  ADMIN_DELETE_TESTIMONIAL_RESET,
+  ADMIN_DELETE_TESTIMONIAL_SUCCESS,
+  CLEAR_ERRORS,
+} from '../constants/testimonialConstant';
+
+export const newTestimonialReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ADD_TESTIMONIAL_REQUEST:
+      return {
+        loading: true,
+        ...state,
+      };
+
+    case ADD_TESTIMONIAL_SUCCESS:
+      return {
+        loading: false,
+        success: action.payload.success,
+        testimoninal: action.payload.data,
+      };
+
+    case ADD_TESTIMONIAL_FAILURE:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const allTestimonialsReducer = (
+  state = { testimoninals: [] },
+  action
+) => {
+  switch (action.type) {
+    case ALL_TESTIMONIALS_REQUEST:
+      return {
+        loading: true,
+      };
+
+    case ALL_TESTIMONIALS_SUCCESS:
+      return {
+        loading: false,
+        testimoninals: action.payload,
+      };
+
+    case ALL_TESTIMONIALS_FAILURE:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const adminTestimonial = (state = {}, action) => {
+  switch (action.type) {
+    case ADMIN_DELETE_TESTIMONIAL_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case ADMIN_DELETE_TESTIMONIAL_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isDeleted: action.payload,
+      };
+
+    case ADMIN_DELETE_TESTIMONIAL_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case ADMIN_DELETE_TESTIMONIAL_RESET:
+      return {
+        ...state,
+        isDeleted: false,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
