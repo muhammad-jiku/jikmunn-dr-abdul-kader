463f61202dd56e0f138fd9e70a16c957ca3981be
muhammad-jiku <muhammadjiku364@gmail.com>
2023-09-12 12:41:11 +0600

    development error handling commented

--- server/index.js
+++ server/index.js
@@ -5,13 +5,13 @@ const cloudinary = require('cloudinary');
 // port
 const port = process.env.PORT || 5000;
 
-// development purpose error handling =>
-// handling uncaught exception
-process.on('uncaughtException', (err) => {
-  console.log(`Error: ${err.message}`);
-  console.log(`Shutting down the server due to Uncaught Exception`);
-  process.exit(1);
-});
+// // development purpose error handling =>
+// // handling uncaught exception
+// process.on('uncaughtException', (err) => {
+//   console.log(`Error: ${err.message}`);
+//   console.log(`Shutting down the server due to Uncaught Exception`);
+//   process.exit(1);
+// });
 
 // connecting to database
 connectToDB();
@@ -28,13 +28,13 @@ const server = app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
-// deveopment purpose error handling =>
-// if express fail to handle any error for that there's global errorHandler: Unhandled Promise Rejection
-process.on('unhandledRejection', (err) => {
-  console.log(`Error: ${err.message}`);
-  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+// // deveopment purpose error handling =>
+// // if express fail to handle any error for that there's global errorHandler: Unhandled Promise Rejection
+// process.on('unhandledRejection', (err) => {
+//   console.log(`Error: ${err.message}`);
+//   console.log(`Shutting down the server due to Unhandled Promise Rejection`);
 
-  server.close(() => {
-    process.exit(1);
-  });
-});
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
